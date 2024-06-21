ce934c8a66c7e6efa97e88fa3f2eb4af3b7741e0
Christian <bournecreative@gmail.com>
2024-06-21 00:12:21 -0500

    Create components folder and Header component, import into the layout file to ensure Header is displayed across all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import Link from 'next/link'
-
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
    <div>
-    <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-    </div>
     Home Page
    </div>
   );
